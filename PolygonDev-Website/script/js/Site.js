3c3b812daaefe002b0255c94552191d852571640
ChiAkuma <ChiAkuma@gmail.com>
2018-06-06 15:54:44 +0200

    Update V0.3.1

--- PolygonDev-Website/script/js/Site.js
+++ PolygonDev-Website/script/js/Site.js
@@ -1,9 +1,7 @@
 $.loadPage = function(where, pageurl, title) {
-    alert("1");
     $(where).load(pageurl);
-    alert("2");
+    
     if (!title == "-1") {
         $("title").html(title);
 	}
-	alert("3");
 }
