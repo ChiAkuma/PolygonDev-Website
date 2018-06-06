1b89fa47086de603202a6a3b64b4340adcb13d18
ChiAkuma <ChiAkuma@gmail.com>
2018-06-06 12:49:33 +0200

    Update V0.3 Test loginarea Fehlschlag ... neu testem ...

--- PolygonDev-Website/script/js/Site.js
+++ PolygonDev-Website/script/js/Site.js
@@ -1,7 +1,9 @@
 $.loadPage = function(where, pageurl, title) {
+    alert("1");
     $(where).load(pageurl);
-    
+    alert("2");
     if (!title == "-1") {
         $("title").html(title);
 	}
+	alert("3");
 }
